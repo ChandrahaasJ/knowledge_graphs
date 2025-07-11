0baf937772d1c7cd579c8b7c3b992914b2a0a881
chandrahaasJ <chandrahaasjasti@yahoo.com>
2025-07-11 12:33:54 +0530

    testing

---
+++ ContextManager.py
@@ -0,0 +1,13 @@
+import networkx as nx
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+    index:int
+    data:str
+    perception:str
+
+
+class ContextManager:
+    def __init__():
+        n=Node()
